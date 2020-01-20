3c3f9bb4f0cc98fdfc9ef2f1937cf5a8e60036ad
kremerdesign <kremerdesign@gmail.com>
2020-01-19 16:29:38 -0800

    Twitter haiku_bot as lambda function - initial commit

---
+++ create_tweet.py
@@ -0,0 +1,91 @@
+import json
+import random
+
+# from botocore.vendored import requests  # for AWS lambda
+import requests  # for local development
+import sys
+import time
+import tweepy
+
+from credentials import *  # use this one for testing
+
+# use this for production; set vars in heroku dashboard
+# from os import environ
+# CONSUMER_KEY = environ['CONSUMER_KEY']
+# CONSUMER_SECRET = environ['CONSUMER_SECRET']
+# ACCESS_KEY = environ['ACCESS_KEY']
+# ACCESS_SECRET = environ['ACCESS_SECRET']
+
+HAIKU_API = "https://haiku.kremer.dev"
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+tweepy_api = tweepy.API(auth)
+
+
+def respond(err, res=None):
+    print(res)  # adds the generated Haiku to the CloudWatch logs
+    return {
+        "statusCode": 400 if err else 200,
+        "body": err.message if err else json.dumps(res),
+        "headers": {"Content-Type": "application/json",},
+    }
+
+
+def lambda_handler(event, context):
+
+    pg = HaikuBot()
+    return respond(None, pg.run())
+
+
+class HaikuBot:
+    def __init__(self, woeid: int = 23424977):
+        self.woeid = woeid
+        self.trends = self.get_twitter_trends(self.woeid)
+
+    def run(self):
+        keywords = self.get_trends()
+        keyword = random.choice(
+            [keyword for keyword in keywords if keyword.isalpha() and keyword[1:].islower()]
+        )
+        generated_haiku = request_haiku(keyword)
+        full_haiku = format_haiku(keyword, generated_haiku)
+
+        tweepy_api.update_status(" \n".join(full_haiku))
+        print(" \n".join(full_haiku))
+
+    def request_haiku(self, keyword):
+        r = requests.get(HAIKU_API, {"keyword": keyword})
+        return r.json()
+
+    def parse_trends(self, full_trend):
+        results = []
+        if " " in full_trend:
+            return full_trend.split(" ")
+        # elif multiple capitals, split on those.
+
+    def get_twitter_trends(self, woeid: int = 23424977):
+        trends = tweepy_api.trends_place(woeid)
+        keywords = []
+        for trend in trends[0]["trends"]:
+            name = trend["name"]
+            if name[0] == "#":
+                name = name[1:]
+            keywords.append(name)
+        return keywords
+
+    def format_haiku(self, keyword, generated_haiku: list):
+        return ["#" + keyword, "-" * 2 * len(keyword)] + generated_haiku
+
+
+def main():
+    """ To run this python script locally:
+    format:  $ python3 create_tweet.py
+    """
+
+    pg = HaikuBot()
+    print(pg.run())
+
+
+if __name__ == "__main__":
+    main()
